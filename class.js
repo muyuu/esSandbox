0f7d916b9e722ed5c594c89d767b1593e18c7611
muyuu <anticyborg@gmail.com>
2017-01-17 09:04:01 +0900

    add class verification file

---
+++ class.js
@@ -0,0 +1,31 @@
+class Sample {
+    constructor(){
+        // this function is Sample's ownProperty
+        this['b'] = ()=>{
+            console.log('method b');
+        };
+
+        // this function is Sample's prototype method
+        this.__proto__['c'] = ()=>{
+            console.log('method c');
+        };
+    }
+
+    // this function is Sample's prototype method
+    a(){
+        console.log('method a');
+    }
+}
+
+// this function is Sample's prototype method
+Sample.prototype['d'] = function(){
+    console.log('method d');
+};
+
+const instance = new Sample();
+const isProto = str => !instance.hasOwnProperty(str);
+const prefTxt = str => `method ${str} is prototype method?`;
+console.log( [prefTxt("a"), isProto("a")].join(" ") );
+console.log( [prefTxt("b"), isProto("b")].join(" ") );
+console.log( [prefTxt("c"), isProto("c")].join(" ") );
+console.log( [prefTxt("d"), isProto("d")].join(" ") );
